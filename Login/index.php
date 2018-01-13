926251552c2761057c3c51493c829cac63dd8ff0
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-01-13 18:21:44 +0700

    Edited form login

--- Login/index.php
+++ Login/index.php
@@ -1,3 +1,4 @@
+<?php include "../db/php-mysql.php" ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,13 +6,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../public/css/bootstrap.min.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="../public/css/styles_login.css">
+    <title>Login</title>
 </head>
 <body>
+    <div class="wrapper">
     <form class='form-login' action="" method='POST'>
-        <div class="row">
-            <h1>Login</h1>
+        <div class="container">
+            <div class="row">
+                <h2 class='text-primary mb-3'>Login</h2>
+            </div>
+            <div class="row">
+                <input class='form-control mb-3' type="text" placeholder='Email' name='email'>
+            </div>
+            <div class="row">
+                <input class='form-control mb-3' type="text" placeholder='Password' name='password'>
+            </div>
+            <div class="row">
+                <label class="mb-3"><input type="checkbox" value="remember" name="remember">&nbsp&nbsp Remember me</label>
+            </div>
+            <div class="row">
+                <input class='btn btn-primary btn-block mb-3' type="submit" value='Sign Up'>
+                <a href="#">forgot Password?</a>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
+                <a href="#">Create account</a>
+            </div>
         </div>
     </form>
+    </div>
 </body>
 </html>
