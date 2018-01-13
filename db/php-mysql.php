54fa4ab9abeb2dc86a0dcb483ab5a45c3b9b6da8
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-01-13 14:59:22 +0700

    Edited connect database

--- db/php-mysql.php
+++ db/php-mysql.php
@@ -9,8 +9,14 @@
     if(!$conn){
         die('không thể kết nối: ' . mysql_error());
     }
+    //tạo database
+    // $sql = "create database php_mysql";
+    // if(!mysqli_query($conn, $sql)){
+    //     die('tạo thất bại');
+    // }
+    // echo 'tạo thành công';
     // echo 'kết nối thành công';
-    // //ngắt kết nối
+    //ngắt kết nối
     // mysqli_close($conn);
 
 ?>
