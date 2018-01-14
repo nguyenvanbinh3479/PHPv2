4c1d84f5e53a04220d6898964420c6d7098293c5
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-01-14 16:23:29 +0700

    Edited delete

--- Login/delete1.php
+++ Login/delete1.php
@@ -1,5 +1,17 @@
+<?php include "../db/php-mysql.php"?>
 <?php
 
-    
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $sql = "delect from user where id=$id";
+        if($conn -> query($sql) ===TRUE){
+            echo "Delete successful ";
+            echo "<a href='main.php'>Back</a>";
+        }else{
+            echo "Error deleting record: " . $conn->error;
+        }
+    }else{
+        echo "You are hacker";
+      }
 
 ?>
