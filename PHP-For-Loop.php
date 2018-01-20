f7dda519bfbda9d827f5afc076fc1a70905e0c65
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-01-20 14:34:38 +0700

    Edited multiplication table

--- PHP-For-Loop.php
+++ PHP-For-Loop.php
@@ -53,3 +53,44 @@
         echo '<br>';  
       } 
     ?>
+<!-- tinh giai thua -->
+  <h1>tinh giai thua</h1>
+    <?php
+      $n = 7;
+      $x = 1;
+      for($i = 1; $i<= $n-1; $i++)
+      {
+        $x *= ($i + 1);
+      }
+      echo "giai thua cua $n = $x <br>";
+    ?>
+<!-- dem 1 ky tu xuat hien bao nhieu lan trong chuoi -->
+  <h1>dem ky tu trong chuoi</h1>
+    <?php
+      $sbd = "binhnguyen";
+      $ktcd = "n";
+      $count = 0;
+      for ($x = 0; $x < strlen($sbd); $x++)
+      {
+        if(substr($sbd, $x, 1) == $ktcd)
+        {
+          $count += 1;
+        }
+      }
+      echo "co " .$count. " ky tu " .$ktcd. " trong chuoi ' " .$sbd." '"; 
+    ?>
+<!-- bang cuu chuong -->
+  <h1>bang cuu chuong</h1>
+    <table align="left" border="1" cellpadding="3" cellspacing="0">  
+      <?php  
+        for ($i = 1; $i <= 10; $i++)  
+        {
+          echo "<tr>";
+          for($j = 2; $j <= 9; $j++)
+          {
+            echo "<td> $j * $i = ".$j * $i."</td>";
+          }
+          echo "</tr>"; 
+        }
+      ?>  
+    </table> 
