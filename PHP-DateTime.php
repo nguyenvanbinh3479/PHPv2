3433af5564f87d41d5f602ee906d4caa917bd3b0
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-01-20 20:13:32 +0700

    Edited datetime

--- PHP-DateTime.php
+++ PHP-DateTime.php
@@ -10,3 +10,21 @@
       $songay = (int)($hieuso/86400);
       print "con $songay nua la toi sinh nhat roi"; 
     ?>
+<!-- in ngay theo dinh dang -->
+  <h1>in ngay theo dinh dang</h1>
+    <?php
+      echo date("Y/m/d").'<br>';
+      echo date("y.m.d").'<br>';
+      echo date("d-m-y").'<br>';
+    ?>
+<!-- tinh khoang cach 2 ngay bat ky -->
+  <h1>tinh khoang cach 2 ngay</h1>
+    <?php
+      $ngaydau = "1997-10-27";
+      $ngaykt = "2018-1-20";
+      $hieuso = abs(strtotime($ngaykt) - strtotime($ngaydau));
+      $nam = floor($hieuso / (365 * 60 * 60 * 24));
+      $thang = floor(($hieuso - $nam * 365 * 60 * 60 * 24) / (30 * 60 * 60 * 24));
+      $ngay = floor(($hieuso - $nam * 365 * 60 * 60 * 24 - $thang * 30 * 60 * 60 * 24) / (60 * 60 *24));
+      printf("%d nam, %d thang, %d ngay", $nam, $thang, $ngay);
+    ?>
