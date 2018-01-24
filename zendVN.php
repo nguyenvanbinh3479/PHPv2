f32eb0ecd95cdcdeba7df7d2376e8a87f0d7ce84
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-01-24 19:25:52 +0700

    Edited function

--- zendVN.php
+++ zendVN.php
@@ -489,272 +489,304 @@
 					?>
 				</div>
 			<!-- may atm -->
-			 <style>
-				.content11 {
-				width: 700px;
-				border: 2px solid #9A9A9A;
-				background: #E6E6E6;
-				margin: 20px auto;
-				}
-				
-				.content11 .info {
-				height: 150px;
-				padding: 0px;
-				text-align: center;
-				} 
-				.content11 .info img {
-				margin: 10px;
-				float: left;
-				border-radius: 5px;
-				}
+			 	<style>
+					.content11 {
+					width: 700px;
+					border: 2px solid #9A9A9A;
+					background: #E6E6E6;
+					margin: 20px auto;
+					}
+					
+					.content11 .info {
+					height: 150px;
+					padding: 0px;
+					text-align: center;
+					} 
+					.content11 .info img {
+					margin: 10px;
+					float: left;
+					border-radius: 5px;
+					}
 
-				.content11 .info h1 {
-				color: red;
-				margin: 0px;
-				}
+					.content11 .info h1 {
+					color: red;
+					margin: 0px;
+					}
 
-				.content11 .info p {
-				font-size: 18px;
-				}
+					.content11 .info p {
+					font-size: 18px;
+					}
 
-				.content11 .info input {
-				height: 30px;
-				}
+					.content11 .info input {
+					height: 30px;
+					}
 
-				.content11 .info input[type='text'] {
-				width: 300px;
-				font-size: 18px;
-				}
+					.content11 .info input[type='text'] {
+					width: 300px;
+					font-size: 18px;
+					}
 
-				.content11 .info input[type='submit'] {
-				width: 100px;
-				font-size: 18px;
-				border-radius: 5px;
-				height: 34px;
-				}
+					.content11 .info input[type='submit'] {
+					width: 100px;
+					font-size: 18px;
+					border-radius: 5px;
+					height: 34px;
+					}
 
-				.clr {
-				clear:both;
-				}
+					.clr {
+					clear:both;
+					}
 
-				.result div p {
-				display: inline-table;
-				font-size: 18px;
-				font-weight: bold;
-				margin-top: 0px;
-				margin-bottom: 5px;
-				}
-				.result div p.col1{
-				width: 200px;
-				text-align: left;
-				}
-				.result div p.col2{
-				width: 200px;
-				text-align: center;
-				}
-				.result div p.col3{
-				width: 200px;
-				text-align: right;
-				}
-				.result {
-				padding: 10px;
-				}
+					.result div p {
+					display: inline-table;
+					font-size: 18px;
+					font-weight: bold;
+					margin-top: 0px;
+					margin-bottom: 5px;
+					}
+					.result div p.col1{
+					width: 200px;
+					text-align: left;
+					}
+					.result div p.col2{
+					width: 200px;
+					text-align: center;
+					}
+					.result div p.col3{
+					width: 200px;
+					text-align: right;
+					}
+					.result {
+					padding: 10px;
+					}
 
-				.result p.total {
-				text-align: right;
-				font-size: 20px;
-				font-weight: bold;
-				color: #3879D9;
-				}
-			</style>
-			  <div class="content11">
-				<?php
-					$money = 0;
-					$total = 0;
-					if(isset($_POST['money'])) $money = $_POST['money'];
-				?>
+					.result p.total {
+					text-align: right;
+					font-size: 20px;
+					font-weight: bold;
+					color: #3879D9;
+					}
+				</style>
+				<div class="content11">
+					<?php
+						$money = 0;
+						$total = 0;
+						if(isset($_POST['money'])) $money = $_POST['money'];
+					?>
 					<div class="info">
-						<img src="public/images/bocap.png" alt="atm">
-						<h1>Mô phỏng máy ATM</h1>
-						<form action="#" method="post">
-							<p>Vui lòng nhập vào số tiền mà quý khách muốn thực hiện giao dịch</p>
-							<input type="text" name="money" value="<?php echo number_format($money);?>">
-							<input type="submit" value="Rút tiền">
-						</form>
+							<img src="public/images/bocap.png" alt="atm">
+							<h1>Mô phỏng máy ATM</h1>
+							<form action="#" method="post">
+								<p>Vui lòng nhập vào số tiền mà quý khách muốn thực hiện giao dịch</p>
+								<input type="text" name="money" value="<?php echo number_format($money);?>">
+								<input type="submit" value="Rút tiền">
+							</form>
 					</div>
 					<div class="clr"></div>
 					<div class="result">
-						<div class="normal">
-							<p class="col1">Mệnh giá</p>
-							<p class="col2">Số lượng</p>
-							<p class="col3">Thành tiền</p>
-						</div>
-						<div class="clr"></div>
+							<div class="normal">
+								<p class="col1">Mệnh giá</p>
+								<p class="col2">Số lượng</p>
+								<p class="col3">Thành tiền</p>
+							</div>
+							<div class="clr"></div>
 					<?php
-						define("ONE", 1000);
-						define("TWO", 2000);
-						define("FIVE", 5000);
-						define("ONE_0", 10000);
-						define("TWO_0", 20000);
-						define("FIVE_0", 50000);
-						define("ONE_00", 100000);
-						define("TWO_00", 200000);
-						define("FIVE_00", 500000);
+							define("ONE", 1000);
+							define("TWO", 2000);
+							define("FIVE", 5000);
+							define("ONE_0", 10000);
+							define("TWO_0", 20000);
+							define("FIVE_0", 50000);
+							define("ONE_00", 100000);
+							define("TWO_00", 200000);
+							define("FIVE_00", 500000);
 
-						$five00 = 0;
-						$two00 = 0;
-						$one00 = 0;
-						$five0 = 0;
-						$two0 = 0;
-						$one0 = 0;
-						$five = 0;
-						$two = 0;
-						$one = 0;
-						if (is_numeric($money) && $money > 1000)
-						{
-							while($money >= FIVE_00)
+							$five00 = 0;
+							$two00 = 0;
+							$one00 = 0;
+							$five0 = 0;
+							$two0 = 0;
+							$one0 = 0;
+							$five = 0;
+							$two = 0;
+							$one = 0;
+							if (is_numeric($money) && $money > 1000)
 							{
-								$five00 += 1;
-								$money = $money - FIVE_00;
-							}
+								while($money >= FIVE_00)
+								{
+									$five00 += 1;
+									$money = $money - FIVE_00;
+								}
 
-							while($money >= TWO_00)
-							{
-								$two00 += 1;
-								$money = $money - TWO_00;
-							}
+								while($money >= TWO_00)
+								{
+									$two00 += 1;
+									$money = $money - TWO_00;
+								}
 
-							while($money >= ONE_00)
-							{
-								$one00 += 1;
-								$money = $money - ONE_00;
-							}
+								while($money >= ONE_00)
+								{
+									$one00 += 1;
+									$money = $money - ONE_00;
+								}
 
-							while($money >= FIVE_0)
-							{
-								$five0 += 1;
-								$money = $money - FIVE_0;
-							}
+								while($money >= FIVE_0)
+								{
+									$five0 += 1;
+									$money = $money - FIVE_0;
+								}
 
-							while($money >= TWO_0)
-							{
-								$two0 += 1;
-								$money = $money - TWO_0;
-							}
+								while($money >= TWO_0)
+								{
+									$two0 += 1;
+									$money = $money - TWO_0;
+								}
 
-							while($money >= ONE_0)
-							{
-								$one0 += 1;
-								$money = $money - ONE_0;
-							}
+								while($money >= ONE_0)
+								{
+									$one0 += 1;
+									$money = $money - ONE_0;
+								}
 
-							while($money >= ONE)
-							{
-								$one += 1;
-								$money = $money - ONE;
-							}
+								while($money >= ONE)
+								{
+									$one += 1;
+									$money = $money - ONE;
+								}
 
-							while($money >= TWO)
-							{
-								$two += 1;
-								$money = $money - TWO;
-							}
+								while($money >= TWO)
+								{
+									$two += 1;
+									$money = $money - TWO;
+								}
 
-							while($money >= FIVE)
-							{
-								$five += 1;
-								$money = $money - FIVE;
-							}
+								while($money >= FIVE)
+								{
+									$five += 1;
+									$money = $money - FIVE;
+								}
 
-							$total = FIVE_00 * $five00 + TWO_00 * $two00 + ONE_00 * $one00 + FIVE_0 * $five0 + TWO_0 * $two0 + ONE_0 * $one0 + ONE * $one + TWO * $two + FIVE * $five;
-						}
+								$total = FIVE_00 * $five00 + TWO_00 * $two00 + ONE_00 * $one00 + FIVE_0 * $five0 + TWO_0 * $two0 + ONE_0 * $one0 + ONE * $one + TWO * $two + FIVE * $five;
+							}
 					?>
 					<?php
-						if($five00 > 0)
-						{
-							echo '<div class="normal">
-											<p class="col1">'.number_format(FIVE_00).'</p>
-											<p class="col2">'.$five00.'</p>
-											<p class="col3">'.number_format(FIVE_00 * $five00).'</p>
-										</div><div class="clr"></div>';
-						}
+							if($five00 > 0)
+							{
+								echo '<div class="normal">
+												<p class="col1">'.number_format(FIVE_00).'</p>
+												<p class="col2">'.$five00.'</p>
+												<p class="col3">'.number_format(FIVE_00 * $five00).'</p>
+											</div><div class="clr"></div>';
+							}
 
-						if($two00 > 0)
-						{
-							echo '<div class="normal">
-											<p class="col1">'.number_format(TWO_00).'</p>
-											<p class="col2">'.$two00.'</p>
-											<p class="col3">'.number_format(TWO_00 * $two00).'</p>
-										</div><div class="clr"></div>';
-						}
+							if($two00 > 0)
+							{
+								echo '<div class="normal">
+												<p class="col1">'.number_format(TWO_00).'</p>
+												<p class="col2">'.$two00.'</p>
+												<p class="col3">'.number_format(TWO_00 * $two00).'</p>
+											</div><div class="clr"></div>';
+							}
 
-						if($one00 > 0)
-						{
-							echo '<div class="normal">
-											<p class="col1">'.number_format(ONE_00).'</p>
-											<p class="col2">'.$one00.'</p>
-											<p class="col3">'.number_format(ONE_00 * $one00).'</p>
-										</div><div class="clr"></div>';
-						}
-						
-						if($five0 > 0)
-						{
-							echo '<div class="normal">
-											<p class="col1">'.number_format(FIVE_0).'</p>
-											<p class="col2">'.$five0.'</p>
-											<p class="col3">'.number_format(FIVE_0 * $five0).'</p>
-										</div><div class="clr"></div>';
-						}
+							if($one00 > 0)
+							{
+								echo '<div class="normal">
+												<p class="col1">'.number_format(ONE_00).'</p>
+												<p class="col2">'.$one00.'</p>
+												<p class="col3">'.number_format(ONE_00 * $one00).'</p>
+											</div><div class="clr"></div>';
+							}
+							
+							if($five0 > 0)
+							{
+								echo '<div class="normal">
+												<p class="col1">'.number_format(FIVE_0).'</p>
+												<p class="col2">'.$five0.'</p>
+												<p class="col3">'.number_format(FIVE_0 * $five0).'</p>
+											</div><div class="clr"></div>';
+							}
 
-						if($two0 > 0)
-						{
-							echo '<div class="normal">
-											<p class="col1">'.number_format(TWO_0).'</p>
-											<p class="col2">'.$two0.'</p>
-											<p class="col3">'.number_format(TWO_0 * $two0).'</p>
-										</div><div class="clr"></div>';
-						}
+							if($two0 > 0)
+							{
+								echo '<div class="normal">
+												<p class="col1">'.number_format(TWO_0).'</p>
+												<p class="col2">'.$two0.'</p>
+												<p class="col3">'.number_format(TWO_0 * $two0).'</p>
+											</div><div class="clr"></div>';
+							}
 
-						if($one0 > 0)
-						{
-							echo '<div class="normal">
-											<p class="col1">'.number_format(ONE_0).'</p>
-											<p class="col2">'.$one0.'</p>
-											<p class="col3">'.number_format(ONE_0 * $one0).'</p>
-										</div><div class="clr"></div>';
-						}
+							if($one0 > 0)
+							{
+								echo '<div class="normal">
+												<p class="col1">'.number_format(ONE_0).'</p>
+												<p class="col2">'.$one0.'</p>
+												<p class="col3">'.number_format(ONE_0 * $one0).'</p>
+											</div><div class="clr"></div>';
+							}
 
-						if($one > 0)
-						{
-							echo '<div class="normal">
-											<p class="col1">'.number_format(ONE).'</p>
-											<p class="col2">'.$one.'</p>
-											<p class="col3">'.number_format(ONE * $one).'</p>
-										</div><div class="clr"></div>';
-						}
+							if($one > 0)
+							{
+								echo '<div class="normal">
+												<p class="col1">'.number_format(ONE).'</p>
+												<p class="col2">'.$one.'</p>
+												<p class="col3">'.number_format(ONE * $one).'</p>
+											</div><div class="clr"></div>';
+							}
 
-						if($two > 0)
-						{
-							echo '<div class="normal">
-											<p class="col1">'.number_format(TWO).'</p>
-											<p class="col2">'.$two.'</p>
-											<p class="col3">'.number_format(TWO * $two).'</p>
-										</div><div class="clr"></div>';
-						}
+							if($two > 0)
+							{
+								echo '<div class="normal">
+												<p class="col1">'.number_format(TWO).'</p>
+												<p class="col2">'.$two.'</p>
+												<p class="col3">'.number_format(TWO * $two).'</p>
+											</div><div class="clr"></div>';
+							}
 
-						if($five > 0)
-						{
-							echo '<div class="normal">
-											<p class="col1">'.number_format(FIVE).'</p>
-											<p class="col2">'.$five.'</p>
-											<p class="col3">'.number_format(FIVE * $five).'</p>
-										</div><div class="clr"></div>';
-						}
-						
-							echo '<hr><p class="total">Tổng tiền: ' .number_format($total) . '</p>';	
-						
+							if($five > 0)
+							{
+								echo '<div class="normal">
+												<p class="col1">'.number_format(FIVE).'</p>
+												<p class="col2">'.$five.'</p>
+												<p class="col3">'.number_format(FIVE * $five).'</p>
+											</div><div class="clr"></div>';
+							}
+							
+								echo '<hr><p class="total">Tổng tiền: ' .number_format($total) . '</p>';	
+							
 					?>
 				</div>
+		</div>
+		<!-- function -->
+		<div class="ham">
+			<style>
+				.ham{
+					width: 800px ;
+					height: auto;
+					border: 2px solid #0cc;
+					margin: 20px auto;
+					padding: 20px;
+				}
+				.ham div{
+					border: 1px solid #0cc;
+					padding: 5px;
+					text-align: center;
+					margin-bottom: 10px;
+				}
+				.ham div p{
+					font-weight: bold;
+				}
+				.ham div p span{
+					font-size: italic;
+					font-weight: normal;
+				}
+			</style>
+			<div style="width: 200px; height: 50px;">
+				<p>box A<span>(200x500)</span></p>
+			</div>
+
+			<div style="width: 300px; height: 100px;">
+				<p>box B<span>(300x100)</span></p>
+			</div>
 		</div>
 	</body>
 </html>
