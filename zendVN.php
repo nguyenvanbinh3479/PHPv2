c82b5164ee52179be085c10aa3ab47ac791ac59a
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-01-24 17:19:43 +0700

    Edited ATM

--- zendVN.php
+++ zendVN.php
@@ -492,7 +492,6 @@
 			 <style>
 				.content11 {
 				width: 700px;
-				height: 400px;
 				border: 2px solid #9A9A9A;
 				background: #E6E6E6;
 				margin: 20px auto;
@@ -570,7 +569,8 @@
 			</style>
 			  <div class="content11">
 				<?php
-					$money = '';
+					$money = 0;
+					$total = 0;
 					if(isset($_POST['money'])) $money = $_POST['money'];
 				?>
 					<div class="info">
@@ -578,34 +578,182 @@
 						<h1>Mô phỏng máy ATM</h1>
 						<form action="#" method="post">
 							<p>Vui lòng nhập vào số tiền mà quý khách muốn thực hiện giao dịch</p>
-							<input type="text" name="money" value="">
+							<input type="text" name="money" value="<?php echo number_format($money);?>">
 							<input type="submit" value="Rút tiền">
 						</form>
 					</div>
 					<div class="clr"></div>
-						<div class="result">
-							<div class="normal">
-								<p class="col1">Mệnh giá</p>
-								<p class="col2">Số lượng</p>
-								<p class="col3">Thành tiền</p>
+					<div class="result">
+						<div class="normal">
+							<p class="col1">Mệnh giá</p>
+							<p class="col2">Số lượng</p>
+							<p class="col3">Thành tiền</p>
 						</div>
-					<div class="clr"></div>
-					
-					<div class="normal">
-						<p class="col1">500.000</p>
-						<p class="col2">6</p>
-						<p class="col3">3.000.000</p>
-					</div>
-					<div class="clr"></div>
+						<div class="clr"></div>
+					<?php
+						define("ONE", 1000);
+						define("TWO", 2000);
+						define("FIVE", 5000);
+						define("ONE_0", 10000);
+						define("TWO_0", 20000);
+						define("FIVE_0", 50000);
+						define("ONE_00", 100000);
+						define("TWO_00", 200000);
+						define("FIVE_00", 500000);
 
-					<div class="normal">
-						<p class="col1">200.000</p>
-						<p class="col2">2</p>
-						<p class="col3">400.000</p>
-					</div>
-					<hr>
-					<p class="total">Tổng tiền: 234.567.000</p>
-					</div><!-- .result -->
+						$five00 = 0;
+						$two00 = 0;
+						$one00 = 0;
+						$five0 = 0;
+						$two0 = 0;
+						$one0 = 0;
+						$five = 0;
+						$two = 0;
+						$one = 0;
+						if (is_numeric($money) && $money > 1000)
+						{
+							while($money >= FIVE_00)
+							{
+								$five00 += 1;
+								$money = $money - FIVE_00;
+							}
+
+							while($money >= TWO_00)
+							{
+								$two00 += 1;
+								$money = $money - TWO_00;
+							}
+
+							while($money >= ONE_00)
+							{
+								$one00 += 1;
+								$money = $money - ONE_00;
+							}
+
+							while($money >= FIVE_0)
+							{
+								$five0 += 1;
+								$money = $money - FIVE_0;
+							}
+
+							while($money >= TWO_0)
+							{
+								$two0 += 1;
+								$money = $money - TWO_0;
+							}
+
+							while($money >= ONE_0)
+							{
+								$one0 += 1;
+								$money = $money - ONE_0;
+							}
+
+							while($money >= ONE)
+							{
+								$one += 1;
+								$money = $money - ONE;
+							}
+
+							while($money >= TWO)
+							{
+								$two += 1;
+								$money = $money - TWO;
+							}
+
+							while($money >= FIVE)
+							{
+								$five += 1;
+								$money = $money - FIVE;
+							}
+
+							$total = FIVE_00 * $five00 + TWO_00 * $two00 + ONE_00 * $one00 + FIVE_0 * $five0 + TWO_0 * $two0 + ONE_0 * $one0 + ONE * $one + TWO * $two + FIVE * $five;
+						}
+					?>
+					<?php
+						if($five00 > 0)
+						{
+							echo '<div class="normal">
+											<p class="col1">'.number_format(FIVE_00).'</p>
+											<p class="col2">'.$five00.'</p>
+											<p class="col3">'.number_format(FIVE_00 * $five00).'</p>
+										</div><div class="clr"></div>';
+						}
+
+						if($two00 > 0)
+						{
+							echo '<div class="normal">
+											<p class="col1">'.number_format(TWO_00).'</p>
+											<p class="col2">'.$two00.'</p>
+											<p class="col3">'.number_format(TWO_00 * $two00).'</p>
+										</div><div class="clr"></div>';
+						}
+
+						if($one00 > 0)
+						{
+							echo '<div class="normal">
+											<p class="col1">'.number_format(ONE_00).'</p>
+											<p class="col2">'.$one00.'</p>
+											<p class="col3">'.number_format(ONE_00 * $one00).'</p>
+										</div><div class="clr"></div>';
+						}
+						
+						if($five0 > 0)
+						{
+							echo '<div class="normal">
+											<p class="col1">'.number_format(FIVE_0).'</p>
+											<p class="col2">'.$five0.'</p>
+											<p class="col3">'.number_format(FIVE_0 * $five0).'</p>
+										</div><div class="clr"></div>';
+						}
+
+						if($two0 > 0)
+						{
+							echo '<div class="normal">
+											<p class="col1">'.number_format(TWO_0).'</p>
+											<p class="col2">'.$two0.'</p>
+											<p class="col3">'.number_format(TWO_0 * $two0).'</p>
+										</div><div class="clr"></div>';
+						}
+
+						if($one0 > 0)
+						{
+							echo '<div class="normal">
+											<p class="col1">'.number_format(ONE_0).'</p>
+											<p class="col2">'.$one0.'</p>
+											<p class="col3">'.number_format(ONE_0 * $one0).'</p>
+										</div><div class="clr"></div>';
+						}
+
+						if($one > 0)
+						{
+							echo '<div class="normal">
+											<p class="col1">'.number_format(ONE).'</p>
+											<p class="col2">'.$one.'</p>
+											<p class="col3">'.number_format(ONE * $one).'</p>
+										</div><div class="clr"></div>';
+						}
+
+						if($two > 0)
+						{
+							echo '<div class="normal">
+											<p class="col1">'.number_format(TWO).'</p>
+											<p class="col2">'.$two.'</p>
+											<p class="col3">'.number_format(TWO * $two).'</p>
+										</div><div class="clr"></div>';
+						}
+
+						if($five > 0)
+						{
+							echo '<div class="normal">
+											<p class="col1">'.number_format(FIVE).'</p>
+											<p class="col2">'.$five.'</p>
+											<p class="col3">'.number_format(FIVE * $five).'</p>
+										</div><div class="clr"></div>';
+						}
+						
+							echo '<hr><p class="total">Tổng tiền: ' .number_format($total) . '</p>';	
+						
+					?>
 				</div>
 		</div>
 	</body>
