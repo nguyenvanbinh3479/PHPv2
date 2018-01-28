05959067528235c36e61156f127ed521de98d5d9
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-01-28 20:09:40 +0700

    Edited function

---
+++ Question/functionB.php
@@ -0,0 +1,26 @@
+<?php
+
+  $data = file('options.txt') or die('cannot read file');
+
+  array_shift($data);
+
+  $arrO = array();
+  foreach($data as $key => $value)
+  {
+    $tmp = explode('|', $value);
+
+		$questionID = $tmp[0];
+		$optionID = $tmp[1];
+		$answer = $tmp[2];
+		$point = $tmp[3];
+
+		$arrO[$questionID][$optionID]['option'] = $answer;
+		$arrO[$questionID][$optionID]['point'] = $point;
+
+	}
+	
+	echo '<pre>';
+	print_r($arrO);
+	echo '</pre>';
+
+?>
