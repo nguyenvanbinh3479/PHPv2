a3ed7c7f95ad3584cda0e69496d5cb395a8fd703
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-01-31 13:58:22 +0700

    Edited readNumber

--- zendVN.php
+++ zendVN.php
@@ -1965,7 +1965,58 @@
         //html entity
           $str = '<b>php</b> is easy';
           echo 'input: ' . $str . '<br>';
-          echo 'output: ' . htmlspecialchars($str);
+          echo 'output: ' . htmlspecialchars_decode($str) . "<br>";
+          $str1 = '<b>php</b> is easy ©';          
+          echo 'output: ' . htmlentities($str1) . "<br>";
+          $str2 = '<b>php</b> is easy';          
+          echo 'output: ' . strip_tags($str2) . "<br>";
+        //doc so co ba chu so
+          $arrNumber = array(
+            0 => 'không',
+            1 => 'một',
+            2 => 'hai',
+            3 => 'ba',
+            4 => 'bốn',
+            5 => 'năm',
+            6 => 'sáu',
+            7 => 'bảy',
+            8 => 'tám',
+            9 => 'chín'
+          );
+          function readNumber3Digit($number, $dictionaryNumber, $readFull = true)
+          {
+            $number = strval($number);
+            $number = str_pad($number, 3, 0, STR_PAD_LEFT);
+                $digit_0 = substr($number, 2, 1);
+                $digit_00 = substr($number, 1, 1);
+                $digit_000 = substr($number, 0, 1);
+
+                $str_000 = $dictionaryNumber[$digit_000] . ' trăm ';
+                $str_00 = $dictionaryNumber[$digit_00] . ' mươi ';
+                if($digit_00 == 0) $str_00  = 'lẻ ';
+                if($digit_00 == 1) $str_00  = 'mười ';
+                $str_0 = $dictionaryNumber[$digit_0];
+                if($digit_00 > 1 && $digit_0 == 1) $str_0  = 'mốt ';           
+                if($digit_00 > 0 && $digit_0 == 5) $str_0  = 'lăm ';           
+                if($digit_00 == 0 && $digit_0 == 0)
+                {
+                  $str_00 = '';
+                  $str_0 = '';
+                }        
+                if($readFull == false)
+                { 
+                  if($digit_000 == 0 ) $str_000 = '';
+                  if($digit_000 == 0 && $digit_00 == 0) $str_00 = '';
+                }   
+                $result = $str_000 . $str_00 . $str_0;
+                return $result;
+          }
+          
+          $number = 651;
+          $result = readNumber3Digit($number, $arrNumber, false);
+          echo "Input: " . $number . '<br>';
+          echo "Output: " . $result . '<br>';
+          
 			?>
 	</body>
 </html>
