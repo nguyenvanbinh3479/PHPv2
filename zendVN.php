8694a5de3227634420d9318199ba85072c7a60de
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-02-13 10:20:26 +0700

    Edited calendar

--- zendVN.php
+++ zendVN.php
@@ -1013,8 +1013,28 @@
                   <!-- calendar --><?php
                 break;
                 case '10:'?>
-                  <!-- calendar --><?php
-                  
+                  <!-- calendar -->
+                  <!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1">
+                        <title>jQuery UI Datepicker - Default functionality</title>
+                        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+                        <link rel="stylesheet" href="/resources/demos/style.css">
+                        <script src="../public/js/jquery-3.3.1.min.js"></script>
+                        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+                        <script>
+                          $( function() {
+                            $( "#datepicker" ).datepicker();
+                          } );
+                        </script>
+                      </head>
+                      <body>
+                        <p>Date: <input type="text" id="datepicker"></p>
+                      </body>
+                    </html>
+                  <?php
 							}
 						}?>
 			</div>
